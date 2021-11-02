a57a346afe6e659e0ffae855b9c0c553e2921534
Micah Teeuws <tutmirage@gmail.com>
2021-11-02 14:02:42 +0300

    initial commit

---
+++ dex.pas
@@ -0,0 +1,77 @@
+unit dex;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils;
+
+type
+  THeaderItem = class(TObject)
+  private
+  protected
+  public
+    constructor Create();
+    destructor Destroy; override;
+  end;
+
+
+function ReadMagic(S: TStream): string;
+function ReadUint(S: TStream): integer;
+function ReadBytes(S: TStream; Size: integer): string;
+
+implementation
+
+
+constructor THeaderItem.Create();
+begin
+  inherited Create();
+end;
+
+destructor THeaderItem.Destroy;
+begin
+  inherited Destroy;
+end;
+
+function ReadMagic(S: TStream): string;
+var
+  i: integer;
+  b: byte;
+begin
+  Result := '';
+  for i := 1 to 8 do
+  begin
+    b := S.ReadByte;
+    if b = 10 then
+      Result += '\n'
+    else if b = 0 then
+      Result += '\0'
+    else
+      Result += char(b);
+  end;
+end;
+
+function ReadUint(S: TStream): integer;
+begin
+  Result := S.ReadByte + 256 * S.ReadByte + 256 * 256 * S.ReadByte +
+    256 * 256 * 256 * S.ReadByte;
+end;
+
+function ReadBytes(S: TStream; Size: integer): string;
+var
+  i: integer;
+  b: byte;
+begin
+  Result := '';
+  if Size > 0 then
+  begin
+    for i := 1 to Size do
+    begin
+      b := S.readbyte;
+      Result += IntToHex(b, 2);
+    end;
+  end;
+end;
+
+end.
---
+++ unit1.pas
@@ -0,0 +1,98 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, ComCtrls,
+  ShellCtrls, bufstream, dex;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Memo1: TMemo;
+    OpenDialog1: TOpenDialog;
+    PageControl1: TPageControl;
+    ShellListView1: TShellListView;
+    TabSheet1: TTabSheet;
+    TabSheet2: TTabSheet;
+    procedure Button1Click(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+  private
+    procedure Clear();
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+  buf: TBufferedFileStream;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  if OpenDialog1.Execute then
+  begin
+    Clear;
+    Form1.Text := OpenDialog1.FileName;
+    buf := TBufferedFileStream.Create(OpenDialog1.FileName, fmOpenRead);
+    Memo1.Lines.Add('magic: ' + ReadMagic(buf));
+    Memo1.Lines.Add('checksum: ' + IntToHex(ReadUint(buf), 8));
+    Memo1.Lines.Add('signature: ' + ReadBytes(buf, 20));
+    Memo1.Lines.Add('file_size: ' + IntToStr(ReadUint(buf)));
+    Memo1.Lines.Add('header_size: ' + IntToStr(ReadUint(buf)));
+    Memo1.Lines.Add('endian_tag: ' + IntToHex(ReadUint(buf), 8));
+
+    Memo1.Lines.Add('link_size: ' + IntToStr(ReadUint(buf)));
+    Memo1.Lines.Add('link_off: ' + IntToStr(ReadUint(buf)));
+
+    Memo1.Lines.Add('map_off: ' + IntToStr(ReadUint(buf)));
+
+    Memo1.Lines.Add('string_ids_size: ' + IntToStr(ReadUint(buf)));
+    Memo1.Lines.Add('string_ids_off: ' + IntToStr(ReadUint(buf)));
+
+    Memo1.Lines.Add('type_ids_size: ' + IntToStr(ReadUint(buf)));
+    Memo1.Lines.Add('type_ids_off: ' + IntToStr(ReadUint(buf)));
+
+    Memo1.Lines.Add('proto_ids_size: ' + IntToStr(ReadUint(buf)));
+    Memo1.Lines.Add('proto_ids_off: ' + IntToStr(ReadUint(buf)));
+
+    Memo1.Lines.Add('field_ids_size: ' + IntToStr(ReadUint(buf)));
+    Memo1.Lines.Add('field_ids_off: ' + IntToStr(ReadUint(buf)));
+
+    Memo1.Lines.Add('method_ids_size: ' + IntToStr(ReadUint(buf)));
+    Memo1.Lines.Add('method_ids_off: ' + IntToStr(ReadUint(buf)));
+
+    Memo1.Lines.Add('class_defs_size: ' + IntToStr(ReadUint(buf)));
+    Memo1.Lines.Add('class_defs_off: ' + IntToStr(ReadUint(buf)));
+
+    Memo1.Lines.Add('data_size: ' + IntToStr(ReadUint(buf)));
+    Memo1.Lines.Add('data_off: ' + IntToStr(ReadUint(buf)));
+  end;
+end;
+
+
+
+procedure TForm1.Clear();
+begin
+  FreeAndNil(buf);
+  Memo1.Clear;
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  Memo1.Clear;
+end;
+
+
+
+end.
